986a74e370f36b42026dcdf4a5b4d76326555eac
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-21 10:33:21 -0800

    created height function

--- add.c
+++ add.c
@@ -1,6 +1,8 @@
 #include "plate.h"
 
 Node add(Node root, char *plate, char *first, char *last) {
+	//if root is null, then it is an uninitialized segment of the tree
+	//in this case, the plate that is being added will be added to the new spot
 	if(root == NULL) {
 		root = malloc(sizeof(struct node));
 		root -> plate = plate;
@@ -9,10 +11,13 @@ Node add(Node root, char *plate, char *first, char *last) {
 		root -> left = NULL;
 		root -> right = NULL;
 	}
+	//If the root is not null, then it traverses the tree to the spot the entry should go in
+	//if it's greater than the root, places it to the left of the root
 	else if(strcmp(root->plate, plate) > 0) {
 		Node new = add(root->left, plate, first, last);
 		root -> left = new;
 	}
+	//If it's less than the root, places it to the right of the root
 	else if(strcmp(root->plate, plate) < 0) {
 		Node new = add(root->right, plate, first, last);
 		root -> right = new;
--- height.c
+++ height.c
@@ -1,5 +1,18 @@
 #include "plate.h"
 
 int height(Node root) {
+	if(root == NULL) {
+		return(-1);
+	}
 
+	int r = height(root->right);
+	int l = height(root->left);
+
+	if( l > r ) {
+		return(l+1);
+	}
+	else {
+		return(r+1);
+	}
+	
 }
