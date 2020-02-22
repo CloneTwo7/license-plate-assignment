054272bdad626537f467335799035cb01666ca8d
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-21 18:55:15 -0800

    Created a search function

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include "plate.h"
 
 int main() {
-	printf("hello world\n");
 	Node root = NULL;
 	root = add(root, "zebra", "stephan", "colbert");
 	root = add(root, "hello", "donald", "glover");
 	root = add(root, "goodbye", "John", "Smith");
 	root = add(root, "thisisplate", "Jackie","Brown");
+	printf("%s\n", root->left->first);
+	int h = height(root);
+	printf("%d\n",h);
 	
 }
--- search.c
+++ search.c
@@ -1,5 +1,27 @@
 #include "plate.h"
 
 int search(Node root, char *plate, char *first, char *last) {
+	//first check to see node is NULL
+	//if it's null that means it's reached the end of the list and the plate doesn't exist
+	if(root == NULL) {
+		return(0);
+	}
 
+	//if current root's plate is equal to the plate passed to it,
+	//then it loads the first and last values with the values in this node
+	else if((strcmp(root->plate)) == 0) {
+		first = root -> first;
+		last = root -> last;
+		return(1);
+	}
+
+	//if it's not null and not equal to the plate passed to it,
+	//figures out whether the plate should be to the left or right
+	else if((strcmp(root->plate, plate)) > 0) {
+		search(root->left, plate, first, last);
+		
+	}
+	else {
+		search(root->right, plate, first, last);
+	}
 }
