ef6c8ae3cd6da54eb3b464d30896d5dc59994bf6
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-27 14:34:07 -0800

    worked on delete a small amount

--- delete.c
+++ delete.c
@@ -6,26 +6,24 @@ Node delete(Node root, char *plate) {
 	if(search(root, plate, first, last) == 0) {
 		return(root);
 	}
-	Node prev = root;
-
-	//checks to see if the current node is the node being deleted
-	if( ((strcmp(root->plate, plate)) == 0)) {
-		//if the root node of the tree is the function is the plate being deleted
-		//and there are no other nodes on the left of the root node
-		//then it sets the node to the right of the root to be the new root node
-		//and frees the old root's memory
-		if(root -> left == NULL) { 
-			//creates new poniter for old root
-			Node oldRoot = root;
-			//sets old root equal to the node to the right of the original root
-			root = root -> right;
-			//free oldRoot
-			free(oldRoot);
-			//returns new root 
-			return(root);
+	
+	int result = strcmp(root->plate,plate);
+	
+	//checks to see if root is the node being deleted
+	if(result == 0) {
+		if(root->left != NULL) {
+			Node largest = root->left;
+			while(root->right != NULL) {
+				largest = largest->right;
+			}
 		}
-		else if(
 
-		} 
 	}
+	else if(result > 0) {
+		root = delete(root->left, plate);
+	}
+	else {
+		root = delete(root->right, plate);
+	}
+
 }
