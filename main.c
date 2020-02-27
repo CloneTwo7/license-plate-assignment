83416e8f835eb2ef71d2aaf2f95e46eb469d0d3c
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-27 10:34:32 -0800

    made some additions to main and perfected small database

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "plate.h" 
 
-int main() {
+int main(int argc, char **argv) {
 	Node root = NULL;
+	if(argc != 2) {
+		printf("Not the correct amount of arguments\n");
+		return(0);
+	}
+	FILE *fp;
+	fp = fopen(argv[1], "r");
+	printf("argv[1] == %s\n",argv[1]);
 	root = add(root, "R2D-3P0", "Travis", "Peterson");
 	root = add(root, "HELP-M3", "Jon", "Snow");
 	root = add(root, "DND-HLP", "Nott", "Brave");
@@ -13,9 +20,10 @@ int main() {
 	root = add(root, "HRY-QTM", "Todd", "Chavez");
 	printf("height: %d\n", height(root));
 	printf("Balanced: %d\n", balanced(root));
+	printf("---------LNR-------\n");
 	LNR(root);
-	printf("-------------------\n");
+	printf("---------NLR-------\n");
 	NLR(root);
-	printf("-------------------\n");
+	printf("---------LRN-------\n");
 	LRN(root);
 }
