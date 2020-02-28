4ff3a6f094f2561981e5493d9306e2316be86e49
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-27 21:30:22 -0800

    Working add and file reading

--- add.c
+++ add.c
@@ -5,22 +5,23 @@ Node add(Node root, char *plate, char *first, char *last) {
 	//in this case, the plate that is being added will be added to the new spot
 	if(root == NULL) {
 		root = malloc(sizeof(struct node));
-		root -> plate = plate;
-		root -> first = first;
-		root -> last = last;
+		root -> plate = malloc(strlen(plate+1));
+		strcpy(root->plate, plate);
+		root -> first = malloc(strlen(first+1));
+		strcpy(root->first, first);
+		root -> last = malloc(strlen(last+1));
+		strcpy(root->last, last);
 		root -> left = NULL;
 		root -> right = NULL;
 	}
 	//If the root is not null, then it traverses the tree to the spot the entry should go in
 	//if it's greater than the root, places it to the left of the root
 	else if(strcmp(root->plate, plate) > 0) {
-		Node new = add(root->left, plate, first, last);
-		root -> left = new;
+		root -> left = add(root->left, plate, first, last);
 	}
 	//If it's less than the root, places it to the right of the root
 	else if(strcmp(root->plate, plate) < 0) {
-		Node new = add(root->right, plate, first, last);
-		root -> right = new;
+		root -> right = add(root->right, plate, first, last);
 	}
 	return(root);
 }
