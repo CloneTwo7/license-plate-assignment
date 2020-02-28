df05138f4c90417216353d89266bfd1771307674
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-27 21:34:27 -0800

    changed main to use getDat

--- main.c
+++ main.c
@@ -7,18 +7,9 @@ int main(int argc, char **argv) {
 		printf("Wrong number of arguments\n");
 		return(0);
 	}
-	FILE *fp = fopen(argv[1], "r");
-	if(fp == NULL) {
-		printf("Cannot open file\n");
-	}
-	char buffer[300];
-	while(fgets(buffer, 300, fp)!= NULL) {
-		char plate[200], first[200], last[200];
-		sscanf(buffer, "%s" "%s" "%s", plate, first, last);
-		printf("adding %s", plate);
-		root = add(root, plate, first, last);
-	}
-	fclose(fp);
+
+	root = getData(root, argv[1]);
+
 	printf("height: %d\n", height(root));
 	printf("Balanced: %d\n", balanced(root));
 	printf("---------LNR-------\n");
